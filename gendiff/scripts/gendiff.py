ffa63f57de282cdd481d3bc2a010133e55057ab2
r.anisimov.esq@gmail.com <r.anisimov.esq@gmail.com>
2022-09-30 17:49:50 +0500

    gendiff.py created

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from cli import get_parser
+#import json
+
+
+def main():
+    parser=get_parser()
+    
+
+#def get_data(file_path: str):
+#    file_as_dict = json.load(open(file_path))
+#    return file_as_dict
+
+
+#def sort_data(input_file):
+#    return dict(sorted(input_file()))
+
+
+#def generate_diff(first_file, second_file):
+#    return
+
+if __name__ == '__main__':
+        main()
+
+
